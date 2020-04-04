2f63ce28879279d1e53fa8883ed6511b474372be
Ilija Bozhinovski <bozini333@gmail.com>
2020-04-04 20:39:50 +0200

    Initial commit

---
+++ src/com/bozin3/datastructures/Main.java
@@ -0,0 +1,11 @@
+package com.bozin3.datastructures;
+import com.bozin3.datastructures.sorting.SortingAlgorithms;
+ 
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] intArray = {20,35,-15,7,55,1,-21};
+        SortingAlgorithms.simpleSort(intArray);
+    }
+}
