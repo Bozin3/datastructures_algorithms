2340339f65b339b48951d0d3779a6aaf004a2413
Ilija Bozhinovski <bozini333@gmail.com>
2020-04-12 23:57:19 +0200

    - Implemented simple ArrayList

--- src/com/bozin3/datastructuresalgorithms/Main.java
+++ src/com/bozin3/datastructuresalgorithms/Main.java
@@ -1,5 +1,7 @@
 package com.bozin3.datastructuresalgorithms;
 
+import com.bozin3.datastructuresalgorithms.datastructures.ArrayList;
+
 import static com.bozin3.datastructuresalgorithms.algorithms.SortingAlgorithms.*;
 
 public class Main {
@@ -9,11 +11,26 @@ public class Main {
         /*
             Sorting algorithms
          */
-        int[] intArray = {20,35,-15,7,55,1,-21,100};
-        recursiveInsertionSort(intArray, 1);
-        for (int i = 0; i < intArray.length; i++) {
-            System.out.println(intArray[i]);
+//        int[] intArray = {20,35,-15,7,55,1,-21,100};
+//        recursiveInsertionSort(intArray, 1);
+//        for (int i = 0; i < intArray.length; i++) {
+//            System.out.println(intArray[i]);
+//        }
+
+        /*
+            ArrayList
+         */
+
+        ArrayList myArrayList = new ArrayList(5);
+        myArrayList.add("James");
+        myArrayList.add("Steve");
+        myArrayList.add("Tom");
+
+        myArrayList.set(0,"John");
+        myArrayList.add(1,"Doe");
+
+        for(int x = 0; x < myArrayList.size(); x++){
+            System.out.println(myArrayList.get(x));
         }
-        
     }
 }
---
+++ src/com/bozin3/datastructuresalgorithms/datastructures/ArrayList.java
@@ -0,0 +1,130 @@
+package com.bozin3.datastructuresalgorithms.datastructures;
+
+/*
+    Simple ArrayList
+    I am using String objects just for demonstration, but i can be done with generics too
+ */
+public class ArrayList {
+
+    private int capacity = 10; //default
+    private int size = 0; // nextIndex
+    private String[] backingArray;
+
+    public ArrayList(){
+        backingArray = new String[capacity];
+    }
+
+    public ArrayList(int capacity) {
+        this.capacity = capacity;
+        backingArray = new String[capacity];
+    }
+
+    // O(1)
+    public int size() {
+        return size;
+    }
+
+    // O(1)
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    // O(n)
+    public boolean contains(String object) {
+        for(int x = 0; x < size; x++) {
+            if(backingArray[x].equals(object)) {
+                return  true;
+            }
+        }
+        return false;
+    }
+
+    // O(1)
+    public void clear() {
+        backingArray = new String[capacity];
+        size = 0;
+    }
+
+    // O(1)
+    public String get(int index) {
+        return backingArray[index];
+    }
+
+    // O(1)
+    public void set(int index, String object){
+        backingArray[index] = object;
+    }
+
+    // If backing array grows then its O(n), else O(1)
+    public void add(String object) {
+        if(size == capacity) { // grow
+            increaseArraySize();
+        }
+
+        backingArray[size++] = object;
+    }
+
+    // add only works between zero index and last element index
+    // O(n)
+    public void add(int index, String element) {
+        if(index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        if(size == capacity) {
+            increaseArraySize();
+        }
+
+        // Shift elements starting at (index) to right
+        String[] newArray = new String[capacity];
+        int x = 0;
+        int y = 0;
+        while (x <= size) {
+            if(x == index) {
+                newArray[x] = element;
+            } else {
+                newArray[x] = backingArray[y];
+                y++;
+            }
+            x++;
+        }
+        backingArray = newArray;
+        size++;
+    }
+
+    // O(n)
+    public String remove(int index) {
+        String removedItem = backingArray[index];
+        if(index < size) {
+            backingArray[index] = null;
+            if (index != size - 1) {
+                // it can be done with System.arrayCopy(), but i like to do it with for loop :)
+                for (int x = index + 1; x < size; x++) {
+                    backingArray[x - 1] = backingArray[x];
+                }
+            }
+            size--;
+        }
+        return removedItem;
+    }
+
+    // O(n)
+    public int indexOf(Object o) {
+        for(int x = 0; x < size; x++){
+            if(backingArray[x].equals(o)){
+                return x;
+            }
+        }
+        return -1;
+    }
+
+    // O(n)
+    private void increaseArraySize() {
+        capacity*=2;
+        String[] newArray = new String[capacity];
+        for(int x = 0; x < size; x++) {
+            newArray[x] = backingArray[x];
+        }
+        backingArray = newArray;
+    }
+}
