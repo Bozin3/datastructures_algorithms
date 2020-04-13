688c7bf963a5d2de3382c3346adce8349695a609
Ilija Bozhinovski <bozini333@gmail.com>
2020-04-13 23:23:09 +0200

    - Implemented Singly Linked List

--- src/com/bozin3/datastructuresalgorithms/Main.java
+++ src/com/bozin3/datastructuresalgorithms/Main.java
@@ -1,36 +1,24 @@
 package com.bozin3.datastructuresalgorithms;
 
 import com.bozin3.datastructuresalgorithms.datastructures.ArrayList;
+import com.bozin3.datastructuresalgorithms.datastructures.SinglyLinkedList;
 
 import static com.bozin3.datastructuresalgorithms.algorithms.SortingAlgorithms.*;
 
 public class Main {
 
     public static void main(String[] args) {
+        SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
+        singlyLinkedList.add("John");
+        singlyLinkedList.add("Dave");
+        singlyLinkedList.add("Tom");
+        singlyLinkedList.add(1,"Kate");
+        System.out.println(singlyLinkedList.contains("Jerry"));
+        singlyLinkedList.addFirst("Kid");
+        singlyLinkedList.addLast("Jerry");
+        singlyLinkedList.isEmpty();
+        singlyLinkedList.removeFirst();
+        singlyLinkedList.removeLast();
 
-        /*
-            Sorting algorithms
-         */
-//        int[] intArray = {20,35,-15,7,55,1,-21,100};
-//        recursiveInsertionSort(intArray, 1);
-//        for (int i = 0; i < intArray.length; i++) {
-//            System.out.println(intArray[i]);
-//        }
-
-        /*
-            ArrayList
-         */
-
-        ArrayList myArrayList = new ArrayList(5);
-        myArrayList.add("James");
-        myArrayList.add("Steve");
-        myArrayList.add("Tom");
-
-        myArrayList.set(0,"John");
-        myArrayList.add(1,"Doe");
-
-        for(int x = 0; x < myArrayList.size(); x++){
-            System.out.println(myArrayList.get(x));
-        }
     }
 }
---
+++ src/com/bozin3/datastructuresalgorithms/datastructures/SinglyLinkedList.java
@@ -0,0 +1,157 @@
+package com.bozin3.datastructuresalgorithms.datastructures;
+
+public class SinglyLinkedList {
+    private int size;
+    private Node head;
+
+    public boolean isEmpty(){
+        return head == null;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public void add(String object){
+        if(isEmpty()){
+            head = new Node(object,null);
+        }else{
+            Node current = head;
+
+            while(current.getNext() != null){
+                current = current.getNext();
+            }
+
+            current.setNext(new Node(object));
+        }
+        size++;
+    }
+
+    public void add(int index, String object){
+        if(index > size){
+            throw new IndexOutOfBoundsException();
+        }
+
+        if(index == 0){
+            addFirst(object);
+        }else if(index == size - 1){
+            addLast(object);
+        }else{
+            Node current = head;
+            int i = 1;
+            while(i < index){
+                current = current.getNext();
+                i++;
+            }
+            Node newObj = new Node(object);
+            newObj.setNext(current.getNext());
+            current.setNext(newObj);
+        }
+
+        size++;
+    }
+
+    public void addFirst(String object){
+        Node newObj = new Node(object);
+        if(isEmpty()){
+            head = newObj;
+        }else{
+            Node temp = head;
+            head = newObj;
+            newObj.setNext(temp);
+        }
+        size++;
+    }
+
+    public void addLast(String object){
+        add(object);
+    }
+
+    public void clear(){
+        head = null;
+        size = 0;
+    }
+
+    public String getFirst(){
+       return head.getData();
+    }
+
+    public String getLast(){
+        if(isEmpty()){
+            return null;
+        }
+
+        Node current = head;
+        while(current.getNext() != null){
+            current = current.getNext();
+        }
+
+        return current.getData();
+    }
+
+    public String removeFirst(){
+        if(isEmpty()){
+            return null;
+        }
+
+        Node firstItem = head;
+        head = firstItem.getNext();
+        firstItem.setNext(null);
+        size--;
+        return firstItem.getData();
+    }
+
+    public String removeLast(){
+        if(isEmpty()){
+            return null;
+        }
+
+        if(head.getNext() == null){
+            clear();
+            return null;
+        }
+
+        Node current = head;
+        while(current.getNext().getNext() != null){
+            current = current.getNext();
+        }
+        Node removedItem = current.getNext();
+        current.setNext(null);
+        size--;
+        return removedItem.getData();
+    }
+
+    public String get(int index){
+
+        if(index >= size){
+            throw new IndexOutOfBoundsException();
+        }
+
+        Node current = head;
+        int i = 0;
+        while(i < index){
+            current = current.getNext();
+            i++;
+        }
+
+        return current.getData();
+    }
+
+    public boolean contains(String object){
+        if(isEmpty()){
+            return false;
+        }
+
+        boolean found = true;
+        Node current = head;
+        while (!current.getData().equals(object)){
+            current = current.getNext();
+            if(current == null){
+                found = false;
+                break;
+            }
+        }
+
+        return found;
+    }
+}
